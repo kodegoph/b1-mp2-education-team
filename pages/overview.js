99ab85578c0b2465460a609cdd45c34b68c7b409
Kim Robert Rozen <rozenkimrobert@gmail.com>
2021-06-17 03:32:31 +0800

    added images

--- pages/overview.js
+++ pages/overview.js
@@ -315,44 +315,52 @@ function overview() {
             <div className="flex flex-wrap">
               <div className="w-full lg:w-8/12 md:pr-6 ">
                 <div className="flex flex-wrap">
-                  <div className="w-full lg:w-6/12 md:pr-6 mb-8 md:mb-0">
-                    <div className="bg-gray-100 rounded-xl p-4">
-                      <div className=" bg-gray-700 py-36 w-full h-full rounded-3xl"></div>
-                      <div></div>
-                    </div>
-                    <div className=" bg-white py-8 px-8 rounded-3xl -mt-12 mx-2 ">
-                      <div className="md:flex md:justify-evenly mb-4">
-                        <div className="mb-4 md:mb-0">
-                          <span className="text-xl font-bold">
-                            Programming - Exam
-                          </span>
-                        </div>
-                        <div className="mb-4 md:mb-0">
-                          <span className="md:pt-4 text-yellow-400 text-sm">
-                            Details
-                          </span>
-                        </div>
+                  <div className="w-full lg:w-6/12 md:pr-6 mb-8 md:mb-0 relative">
+                    <div className="bg-gray-100 rounded-xl p-4 ">
+                      <div className="   w-full h-full rounded-3xl ">
+                        <img
+                          className="rounded-3xl "
+                          src="/img/programming-exam.jpg"
+                          alt=""
+                        />
                       </div>
-                      <div className="flex">
-                        <div className="md:ml-16 mr-2">
-                          <span>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="20"
-                              height="20"
-                              fill="currentColor"
-                              class="bi bi-alarm"
-                              viewBox="0 0 16 16"
-                            >
-                              <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
-                              <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z" />
-                            </svg>
-                          </span>
+                    </div>
+
+                    <div className="absolute bottom-0 -left-3 w-full">
+                      <div className=" bg-white py-8 px-8 rounded-3xl mx-5">
+                        <div className="md:flex md:justify-evenly mb-4">
+                          <div className="mb-4 md:mb-0 ">
+                            <span className="text-xl font-bold ">
+                              Programming - Exam
+                            </span>
+                          </div>
+                          <div className="mb-4 md:mb-0">
+                            <span className="md:pt-4 text-yellow-400 text-sm">
+                              Details
+                            </span>
+                          </div>
                         </div>
-                        <span className="text-sm text-gray-400 mr-4">
-                          On going
-                        </span>
-                        <span className="text-sm">12 March 2021</span>
+                        <div className="flex">
+                          <div className="md:ml-16 mr-2">
+                            <span>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                fill="currentColor"
+                                class="bi bi-alarm"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
+                                <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z" />
+                              </svg>
+                            </span>
+                          </div>
+                          <span className="text-sm text-gray-400 mr-4">
+                            On going
+                          </span>
+                          <span className="text-sm">12 March 2021</span>
+                        </div>
                       </div>
                     </div>
                   </div>
